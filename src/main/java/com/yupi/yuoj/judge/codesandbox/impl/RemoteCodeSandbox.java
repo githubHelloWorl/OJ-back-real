b4a8d2c1d66f0dc384c2a86f313c7116d0d7718e
cmd <3417372950@qq.com>
2024-06-23 14:21:51 +0800

    2024-06-23 14:20 设置remote docker

--- src/main/java/com/yupi/yuoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/yupi/yuoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -8,11 +8,14 @@ import com.yupi.yuoj.judge.codesandbox.CodeSandbox;
 import com.yupi.yuoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.yupi.yuoj.judge.codesandbox.model.ExecuteCodeResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * 远程代码沙箱（实际调用接口的沙箱）
  */
 public class RemoteCodeSandbox implements CodeSandbox {
+    @Value("${hostname.remote}")
+    private static String host;
 
     // 定义鉴权请求头和密钥
     private static final String AUTH_REQUEST_HEADER = "auth";
@@ -23,7 +26,7 @@ public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        String url = "http://101.37.20.106:8090/executeCode";
+        String url = "http://" + host + ":8090/executeCode";
         String json = JSONUtil.toJsonStr(executeCodeRequest);
         String responseStr = HttpUtil.createPost(url)
                 .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
